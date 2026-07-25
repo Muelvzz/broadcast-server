aedc06918536379692b6caa64c29b3ee1511c569
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-07-25 18:08:31 +0800

    refactor: bug fixes

--- client.js
+++ client.js
@@ -4,33 +4,25 @@ import { promptForMessage } from "./utils/promptForMessage.js"
 
 const ws = new WebSocket(`ws://localhost:${PORT}`)
 
-while (true) {
+const clientInput = prompt("")
+if (clientInput.trim().toLowerCase() === "broadcast-server connect") {
+  console.log("Client connected to the server")
 
-  const clientInput = prompt("")
-
-  if (clientInput.trim().toLowerCase() === "broadcast-server connect") {
-
-    ws.on("open", () => {
-      console.log("Connected to the WebSocket server\n")
-      promptForMessage(ws)
-    })
-    
-    ws.on("message", (message) => {
-      console.log(`Server: ${message}`)
-    })
-    
-    ws.on("error", (error) => {
-      console.error("WebSocket error: ", error)
-    })
-    
-    ws.on("close", () => {
-      console.log("Disconnected from the server")
-      process.exit(0)
-    })
-
-  } else {
-
-    console.log("Invalid input\n")
-
-  }
+  ws.on("open", () => {
+    console.log("Connected to the WebSocket server")
+    promptForMessage(ws)
+  })
+  
+  ws.on("message", (message) => {
+    console.log(`Server: ${message}`)
+  })
+  
+  ws.on("error", (error) => {
+    console.error("WebSocket error: ", error)
+  })
+  
+  ws.on("close", () => {
+    console.log("Disconnected from the server")
+    process.exit(0)
+  })
 }
--- server.js
+++ server.js
@@ -5,37 +5,22 @@ import { PORT, prompt } from "./core/config.js"
 const wss = new WebSocketServer({ port: PORT })
 
 console.log("Server initialized\n")
-
-while (true) {
-
-  const serverInput = prompt("")
   
-  if (serverInput.trim().toLowerCase() === "broadcast-server start") {
-    console.log("Broadcast Server started\n")
+const serverInput = prompt("")
+
+if (serverInput.trim().toLowerCase() === "broadcast-server start") {
+  console.log("Broadcast Server started\n")
+
+  wss.on("connection", (ws) => {
+    console.log("New client connected");
   
-    wss.on("connection", (ws) => {
-      console.log("New client connected");
-      ws.send("Welcome to the 'broadcast-server' Server")
-    
-      ws.on("message", (message) => {
-        console.log(`Received: ${message}`)
-        ws.send(`Server received: ${message}`)
-      })
-    
-      ws.on("close", () => {
-        console.log("Client disconnected")
-      })
+    ws.on("message", (message) => {
+      console.log(`Received: ${message}`)
     })
   
-  } else if (serverInput.length === 0) {
-  
-    console.log("Server stop")
-    process.exit(0)
-  
-  } else {
-
-    console.log("Invalid input\n")
-
-  }
+    ws.on("close", () => {
+      console.log("Client disconnected")
+    })
+  })
 
 }
