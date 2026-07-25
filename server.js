390544f4148f2425211cf4fd3f868903265912ed
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-07-25 20:08:54 +0800

    development: creating default user names when different clients are connecting

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 import { WebSocketServer } from "ws"
+import { randomUUID } from "crypto"
 
 import { PORT, prompt } from "./core/config.js"
 
 const wss = new WebSocketServer({ port: PORT })
+let connectedCount = 0
+let userList = []
 
 console.log("Server initialized\n")
   
 const serverInput = prompt("")
 
 if (serverInput.trim().toLowerCase() === "broadcast-server start") {
+
   console.log("Broadcast Server started\n")
 
   wss.on("connection", (ws) => {
-    console.log("New client connected");
+    connectedCount++
+
+    ws.id = randomUUID()
+    ws.userName = `User ${connectedCount}`
+    userList.push(ws.userName)
+
+    console.log(`New client connected as ${ws.userName}`);
   
     ws.on("message", (message) => {
       console.log(`Received: ${message}`)
