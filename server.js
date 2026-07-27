e8b1f16be85fd296f2ab4f4af1976236f6381d15
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-07-27 21:55:59 +0800

    feature: creating random usernames

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import { WebSocketServer, WebSocket } from "ws"
 import { appendLog } from "./utils/appendLog.js"
+import { randomUsername } from "./utils/randomUsername.js"
 
 // create the server socket
 const wss = new WebSocketServer({ port:8080 })
+const activeUsernames = []
 
+// Connection event
 wss.on("connection", (socket, request) => {
   const ip = request.socket.remoteAddress
-  appendLog("[SYSTEM]", `Client ${ip} is connected`)
+  
+  const userName = randomUsername(activeUsernames)
+  activeUsernames.push(userName)
+
+  appendLog("[SYSTEM]", `Client ${ip} connected as ${userName}`)
+
+  console.log("Connected users: ", activeUsernames)
 })
